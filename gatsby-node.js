d40eee51aee7db98d1c3f4f431263018f58e2e0d
Dany Dodson <danydodson@gmail.com>
2021-03-25 03:36:10 -0500

    updates

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
-/* eslint 'no-console': 'off' */
+/* eslint "no-console": "off" */
 
-const path = require('path');
-const _ = require('lodash');
-const moment = require('moment');
-const siteConfig = require('./data/SiteConfig');
+const path = require("path");
+const _ = require("lodash");
+const moment = require("moment");
+const siteConfig = require("./data/SiteConfig");
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
 
@@ -11,46 +11,44 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
   let slug;
 
-  if (node.internal.type === 'MarkdownRemark') {
-
+  if (node.internal.type === "MarkdownRemark") {
     const fileNode = getNode(node.parent);
     const parsedFilePath = path.parse(fileNode.relativePath);
-
     if (
-      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
-      Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
+      Object.prototype.hasOwnProperty.call(node, "frontmatter") &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, "title")
     ) {
       slug = `/${_.kebabCase(node.frontmatter.title)}`;
-    } else if (parsedFilePath.name !== 'index' && parsedFilePath.dir !== '') {
+    } else if (parsedFilePath.name !== "index" && parsedFilePath.dir !== "") {
       slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
-    } else if (parsedFilePath.dir === '') {
+    } else if (parsedFilePath.dir === "") {
       slug = `/${parsedFilePath.name}/`;
     } else {
       slug = `/${parsedFilePath.dir}/`;
     }
 
-    if (Object.prototype.hasOwnProperty.call(node, 'frontmatter')) {
-      if (Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug'))
+    if (Object.prototype.hasOwnProperty.call(node, "frontmatter")) {
+      if (Object.prototype.hasOwnProperty.call(node.frontmatter, "slug"))
         slug = `/${_.kebabCase(node.frontmatter.slug)}`;
-      if (Object.prototype.hasOwnProperty.call(node.frontmatter, 'date')) {
+      if (Object.prototype.hasOwnProperty.call(node.frontmatter, "date")) {
         const date = moment(node.frontmatter.date, siteConfig.dateFromFormat);
         if (!date.isValid)
           console.warn(`WARNING: Invalid date.`, node.frontmatter);
 
-        createNodeField({ node, name: 'date', value: date.toISOString() });
+        createNodeField({ node, name: "date", value: date.toISOString() });
       }
     }
-    createNodeField({ node, name: 'slug', value: slug });
+    createNodeField({ node, name: "slug", value: slug });
   }
 };
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const postPage = path.resolve('src/templates/post.jsx');
-  const tagPage = path.resolve('src/templates/tag.jsx');
-  const categoryPage = path.resolve('src/templates/category.jsx');
-  const listingPage = path.resolve('./src/templates/listing.jsx');
-  const landingPage = path.resolve('./src/templates/landing.jsx');
+  const postPage = path.resolve("src/templates/post.jsx");
+  const tagPage = path.resolve("src/templates/tag.jsx");
+  const categoryPage = path.resolve("src/templates/category.jsx");
+  const listingPage = path.resolve("./src/templates/listing.jsx");
+  const landingPage = path.resolve("./src/templates/landing.jsx");
 
   // Get a full list of markdown posts
   const markdownQueryResult = await graphql(`
