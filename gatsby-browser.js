8e406f85c411c5d3fbd36749e8df304eefa28d6a
Dany Dodson <danydodson@gmail.com>
2021-06-08 18:15:28 -0500

    updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import { wrapRootElement as wrap } from './src/styles/code/wrap-root-element'
+
 import Prism from 'prism-react-renderer/prism'
 ;(typeof global !== 'undefined' ? global : window).Prism = Prism
 
@@ -11,22 +12,15 @@ require('prismjs/plugins/command-line/prism-command-line.css')
 require('prismjs/components/prism-bash')
 require('prismjs/components/prism-csharp')
 require('prismjs/components/prism-css')
-require('prismjs/components/prism-diff')
 require('prismjs/components/prism-docker')
-require('prismjs/components/prism-git')
 require('prismjs/components/prism-graphql')
 require('prismjs/components/prism-http')
 require('prismjs/components/prism-javascript')
 require('prismjs/components/prism-json')
 require('prismjs/components/prism-lua')
-require('prismjs/components/prism-makefile')
 require('prismjs/components/prism-markdown')
 require('prismjs/components/prism-nginx')
-require('prismjs/components/prism-powershell')
-require('prismjs/components/prism-python')
 require('prismjs/components/prism-jsx')
 require('prismjs/components/prism-tsx')
 require('prismjs/components/prism-scss')
 require('prismjs/components/prism-shell-session')
-require('prismjs/components/prism-sql')
-require('prismjs/components/prism-typescript')
