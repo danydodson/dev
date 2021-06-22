1a5c3194f0bfb48e867ab8f7d6904669d91ede05
Dany Dodson <danydodson@gmail.com>
2021-06-22 16:50:21 -0500

    updates

--- src/utils/set-theme.js
+++ src/utils/set-theme.js
@@ -1,5 +1,9 @@
 import { theme } from '../styles/global/theme'
 
 export const setThemeVars = (lightVar, darkVar) => {
-  return theme.curTheme === 'light' ? lightVar : darkVar
+  return (
+    theme.curTheme === 'light'
+      ? lightVar
+      : darkVar
+  )
 }
