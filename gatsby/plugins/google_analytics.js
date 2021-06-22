eae965f58ef21998aeed3950a6f610b8843cf062
Dany Dodson <danydodson@gmail.com>
2021-06-22 18:44:48 -0500

    moved plugin

---
+++ gatsby/plugins/google_analytics.js
@@ -0,0 +1,9 @@
+
+const gatsby_plugin_google_analytics = {
+  resolve: `gatsby-plugin-google-analytics`,
+  options: {
+    trackingId: 'UA-45666519-2',
+  }
+}
+
+module.exports = gatsby_plugin_google_analytics
